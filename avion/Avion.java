9e9198bcf6416a5016f33de06fe93b412f95c4ed
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2022-01-25 18:08:04 -0500

    ordenar codigo

--- avion/Avion.java
+++ avion/Avion.java
@@ -2,13 +2,13 @@ package avion;
 
 public class Avion {
     public static void main(String[] args) {
-        float DeltaTime = 0.09375f;
-        float time = 0.0f;
-        float Vx = 92.25f;
-        float v = 0.0f;
-        float a = 0.625f;
-        float x = 0.0f;
-        float xLimite = 6808.05f;
+        float DeltaTime = (0.09375f);
+        float time = (0.0f);
+        float Vx = (92.25f);
+        float v = (0.0f);
+        float a = (0.625f);
+        float x = (0.0f);
+        float xLimite = (6808.05f);
         int i = 0;
 
         while (x <= xLimite) {
