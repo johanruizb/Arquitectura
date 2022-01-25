1d12772275ff64bf78081724c88c89a9b74a108f
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2022-01-25 12:02:44 -0500

    iteracion

--- estadio/Estadio.java
+++ estadio/Estadio.java
@@ -5,14 +5,20 @@ public class Estadio {
         float pagoxingreso = 0.01718750f;
         int CapacidadEstadio = 48712;
         float DineroAcumulado = 0;
-        int numeroMaxAsistentes = (50 * CapacidadEstadio);
+        int numeroMaxAsistentes = (50 * CapacidadEstadio); // 2435600
         int ConteoAsistentes = 0;
 
-        while (ConteoAsistentes <= numeroMaxAsistentes) {
+        while (ConteoAsistentes < numeroMaxAsistentes) {
             ConteoAsistentes++;
             DineroAcumulado += pagoxingreso;
         }
+        System.out.println("");
+        System.out.println("=============================");
+        //System.out.println("Iteracion " + ConteoAsistentes);
         System.out.println("Girar cheque por: " + DineroAcumulado);
         System.out.println("Pago que deberia haber sido: " + numeroMaxAsistentes * pagoxingreso);
+        System.out.println("=============================");
+        System.out.println("");
+
     }
 }
